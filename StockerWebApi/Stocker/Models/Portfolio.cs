5e8303658414bf3839395930c7f14601cd02e308
johnwunderle <wunderlejohn@yahoo.com>
2019-04-08 12:30:22 -0400

    sqlconnection nuget added

---
+++ StockerWebApi/Stocker/Models/Portfolio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stocker.Models
+{
+    public class Portfolio
+    {
+        public string Symbol { get; set; }
+        public int NumberOfShares { get; set; }
+        public int UserId { get; set; }
+    }
+}
