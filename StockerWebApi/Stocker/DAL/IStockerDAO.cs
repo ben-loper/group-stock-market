cd801f084f592e4bc1df8661e108f81882abcffe
johnwunderle <wunderlejohn@yahoo.com>
2019-04-08 15:01:13 -0400

    script for sql

--- StockerWebApi/Stocker/DAL/IStockerDAO.cs
+++ StockerWebApi/Stocker/DAL/IStockerDAO.cs
@@ -8,5 +8,7 @@ namespace Stocker.DAL
     public interface IStockerDAO
     {
         IList<Portfolio> GetPortfolio(int id);
+
+        bool AddTransaction(int userId, string symbol, int numberOfShares, decimal price, string buyOrSell);
     }
 }
