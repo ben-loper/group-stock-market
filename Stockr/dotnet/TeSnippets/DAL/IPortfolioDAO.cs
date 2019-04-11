d3cd380179a497324597914587df839afbd6a56e
johnwunderle <wunderlejohn@yahoo.com>
2019-04-11 13:51:40 -0400

    portfoliocontroller

---
+++ Stockr/dotnet/TeSnippets/DAL/IPortfolioDAO.cs
@@ -0,0 +1,21 @@
+﻿using StockrWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockrWebApi.DAL
+{
+    public interface IPortfolioDAO
+    {
+        Portfolio FindById(int id, int userid);
+
+        List<Portfolio> GetPortfolio(int userId);
+
+        Portfolio CreatePortfolio(Portfolio portfolio);
+
+        Portfolio UpdatePortfolio(Portfolio portfolio);
+
+        void Delete(int id, int userid);
+    }
+}
