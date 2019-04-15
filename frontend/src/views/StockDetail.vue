99f50fd42749a8039a1342e004b82c214f8b7977
johnwunderle <wunderlejohn@yahoo.com>
2019-04-15 09:36:14 -0400

    more stock detail

--- frontend/src/views/StockDetail.vue
+++ frontend/src/views/StockDetail.vue
@@ -5,6 +5,13 @@
     <div>{{stockInfo.symbol}}</div>
     <div>{{stockInfo.companyName}}</div>
     <div>{{stockInfo.description}}</div>
+    <img v-bind:src="image.url">
+    <div>52-Week Range: {{stats.week52low}} - {{stats.week52high}}</div>
+    <div>Shares Outstanding: {{stats.sharesOutstanding}}</div>
+    <div>Beta: {{stats.beta}}</div>
+    <div>P/E: {{stats.peRatio}}</div>
+    <div>P/S: {{stats.priceToSales}}</div>
+    <div>P/B: {{stats.priceToBook}}</div>
 </div>
 </template>
 
@@ -23,7 +30,9 @@ props:{
   data() {
     return{
       user: null,
-      stockInfo: {}
+      stockInfo: {},
+      image: {},
+      stats: {}
     }
  },
    beforeMount(){
@@ -32,7 +41,7 @@ props:{
   },
     created(){
         //let symbol = document.getElementById('search').value;
-        fetch(`https://cloud.iexapis.com/beta/stock/${symbol}/company?token=pk_cdd72b15fa2a4735897c36067dd39008`)
+        fetch(`https://cloud.iexapis.com/beta/stock/aapl/company?token=pk_cdd72b15fa2a4735897c36067dd39008`)
         .then((response) => {
             return response.json();
         })
@@ -42,8 +51,29 @@ props:{
         
         })
         .catch((err) => console.error(err));
-        
+
+        fetch(`https://cloud.iexapis.com/beta/stock/aapl/logo?token=pk_cdd72b15fa2a4735897c36067dd39008`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((resp) => {
+            this.image= resp;
+            this.$forceUpdate();
+        })
+        .catch((err) => console.error(err));
+
+        fetch(`https://cloud.iexapis.com/beta/stock/aapl/advanced-stats?token=pk_cdd72b15fa2a4735897c36067dd39008`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((resp) => {
+            this.stats= resp;
+            this.$forceUpdate();
+        })
+        .catch((err) => console.error(err));
         },
+
+        
     methods:{
     }
 }
